9cf78fb08dfbec623d2736e71aa251a5941d3957
FedericoSpina <FedericoSpina@hotmail.com>
2022-11-10 15:39:21 -0300

    Nuevamente Router v6 failed
    copy last - nothing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import AuthProvider from "./auth/AuthProvider";
+import "./index.css";
+import Login from "./pages/login/Login";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).root(
+  <>
+    <Login />
+  </>
+);
